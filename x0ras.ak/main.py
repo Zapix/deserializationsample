b504b2cdb640157bcbb7069bdddbeb3d7e4cce6c
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2013-06-25 13:43:06 +0600

    Add json dumps

--- x0ras.ak/main.py
+++ x0ras.ak/main.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import simplejson as json
+
 import deserializators
 
 
@@ -7,12 +9,12 @@ if __name__ == '__main__':
 
     value = '<point><x>1</x><y>2</y><z>3</z></point>'
     print 'First method'
-    print 'Input value: ', value
-    print 'Output value: ',
-    print deserializators.deserialize_to_point_first(value).to_dict()
+    print 'Input value: %s' % value
+    result = deserializators.deserialize_to_point_first(value).to_dict()
+    print 'Output value: %s' % result
 
     value = '<point><values x="4" y="5" z="6"/></point>'
     print 'Second method'
-    print 'Input value: ', value
-    print 'Output value:',
-    print deserializators.deserialize_to_point_second(value).to_dict()
+    print 'Input value: %s' % value
+    result = deserializators.deserialize_to_point_second(value).to_dict()
+    print 'Output value: %s' % json.dumps(result)
--- zapix/main.py
+++ zapix/main.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import simplejson as json
+
 import deserializators
 
 if __name__ == '__main__':
@@ -6,10 +8,12 @@ if __name__ == '__main__':
 
     value = '<point><x>1</x><y>2</y><z>3</z></point>'
     print 'First method'
-    print 'Input value: ', value
-    print 'Output value: ', deserializators.deserialize_point_first(value)
+    print 'Input value: %s' % value
+    result = deserializators.deserialize_point_first(value)
+    print 'Output value: %s' % json.dumps(result)
 
     value = '<point><values x="4" y="5" z="6"/></point>'
     print 'Second method'
-    print 'Input value: ', value
-    print 'Output value:', deserializators.deserialize_point_second(value)
+    print 'Input value: %s' % value
+    result = deserializators.deserialize_point_second(value)
+    print 'Output value: %s' % json.dumps(result)
